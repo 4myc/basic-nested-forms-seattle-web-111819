256db706e8a9ecc3cc327fe744e19bfb1c41deea
4myc <missch3n@gmail.com>
2019-12-24 18:49:52 -0800

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-
+  accepts_nested_attributes_for :addresses
+  # can't use "accepts_nested_attributes_for" when we want to avoid duplicates of the row we're creating
 end
